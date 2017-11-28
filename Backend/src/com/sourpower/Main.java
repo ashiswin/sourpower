afd6931fa03651af769f5282b191a548102881dc
Isaac Ashwin <ashiswin@gmail.com>
2017-11-29 01:58:49 +0800

    fake bug

--- Backend/src/com/sourpower/Main.java
+++ Backend/src/com/sourpower/Main.java
@@ -52,7 +52,7 @@ public class Main extends Application {
         router.attach("/user", UserResource.class);
         router.attach("/friend", FriendResource.class);
         router.attach("/friend/request", FriendRequestResource.class);
-        router.attach("/activity", ActivityResource.class);
+        router.attach("/activit", ActivityResource.class);
         router.attach("/user/authenticate", AuthenticationResource.class);
         router.attach("/control/shutdown", ShutdownResource.class);
         // return the router.
