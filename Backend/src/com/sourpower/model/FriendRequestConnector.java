f3b2d6a2cdcbe2ac4e2e1c7acf2b1a58da3073fe
Isaac Ashwin <ashiswin@gmail.com>
2017-11-29 01:30:01 +0800

    Added friend request connector

---
+++ Backend/src/com/sourpower/model/FriendRequestConnector.java
@@ -0,0 +1,45 @@
+package com.sourpower.model;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class FriendRequestConnector {
+	public static PreparedStatement createStatement = null;
+	public static PreparedStatement selectStatement = null;
+	
+	public static final String TABLE_NAME = "friendrequests";
+	public static final String COLUMN_ID = "id";
+	public static final String COLUMN_REQUESTER = "requester";
+	public static final String COLUMN_REQUESTEE = "requestee";
+	
+	public FriendRequestConnector() {
+		if(SQLProvider.connect == null) {
+			SQLProvider.connect();
+		}
+	}
+	
+	public int create(int requester, int requestee) throws SQLException {
+		if(createStatement == null || createStatement.isClosed()) {
+			createStatement = SQLProvider.connect.prepareStatement("INSERT INTO `" + TABLE_NAME + "`(`" + COLUMN_REQUESTER + "`, `" + COLUMN_REQUESTEE + "`) VALUES(?, ?)");
+		}
+		
+		createStatement.setInt(1, requester);
+		createStatement.setInt(2, requestee);
+		
+		int row = createStatement.executeUpdate();
+		if(row == 0) return -1; // Failed to insert
+		
+		return 1;
+	}
+	
+	public ResultSet select(int userId) throws SQLException {
+		if(selectStatement == null || selectStatement.isClosed()) {
+			selectStatement = SQLProvider.connect.prepareStatement("SELECT * FROM " + TABLE_NAME + " WHERE `" + COLUMN_REQUESTEE + "` = ?");
+		}
+		
+		selectStatement.setInt(1, userId);
+		
+		return selectStatement.executeQuery();
+	}
+}
